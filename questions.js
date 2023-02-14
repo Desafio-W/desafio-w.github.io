e350c11060af6ee81030f2fde74c63af9d58563c
Erackson Brito <erackson@gmail.com>
2023-02-14 17:19:28 +0000

    Creates the SPA and loads the initial 39 questions

---
+++ questions.js
@@ -0,0 +1,159 @@
+var questions = [
+
+    {
+        "q": "Sommelier é:"
+        , "a": "O profissional responsável pelo serviço do vinho"
+    },
+    {
+        "q": "O que é um vinho meia-garrafa?"
+        , "a": "Um vinho com 375ml"
+    },
+    {
+        "q": "O que é Melon de Bourgogne?"
+        , "a": "Uma uva branca do Vale do Loire"
+    },
+    {
+        "q": "Queijos frescos harmonizam melhor com?"
+        , "a": "Brancos leves, rosés de cor clara e espumante brut"
+    },
+    {
+        "q": "Quais são as principais uvas brancas de Bordeaux?"
+        , "a": "Muscadelle, Sauvignon Blanc e Sémillon"
+    },
+    {
+        "q": "Prosecco é de que país?"
+        , "a": "Itália"
+    },
+    {
+        "q": "Provence é famosa por produzir..."
+        , "a": "Vinhos rosés"
+    },
+    {
+        "q": "Em maio de 2020, uma região brasileira recebeu o registro de Indicação de Procedência. Qual região foi essa?"
+        , "a": "Campanha Gaúcha"
+    },
+    {
+        "q": "Qual a uva TINTA de Madiran na França que se tornou destaque no Uruguai?"
+        , "a": "Tannat"
+    },
+    {
+        "q": "O que é Screwcap?"
+        , "a": "Um tipo de vedação - tampa de rosca"
+    },
+    {
+        "q": 'O que é um espumante "Blanc de Noir"?'
+        , "a": "Um espumante branco elaborado com uva(s) tintas (s)."
+    },
+    {
+        "q": "Qual a principal uva TINTA de Valpolicella, na Itália?"
+        , "a": "Corvina"
+    },
+    {
+        "q": "Qual é a principal uva tinta cultivada em Mendonza, na Argentina?"
+        , "a": "Malbec"
+    },
+    {
+        "q": "Qual a melhor forma para guardar o vinho?"
+        , "a": "Local fresco e arejado"
+    },
+    {
+        "q": "O Vinho VIK A, que pode ser encontrado no catálogo da Wine, é produzido em qual país?"
+        , "a": "Chile"
+    },
+    {
+        "q": "As regiões da Califórnia, Monterey e Santa Bárbara são conhecidas pelas uvas:"
+        , "a": "Pinot Noir e Chardonnay"
+    },
+    {
+        "q": "A Gamay é a uva..."
+        , "a": "Utilizada para fazer Beaujolais"
+    },
+    {
+        "q": "O processo que consiste na transformação do ácido málico em ácido lático no vinho, por bactérias é:"
+        , "a": "Malolática"
+    },
+    {
+        "q": "Assinale a correta:"
+        , "a": "Vinho tinto na temperatura ideal de serviço é melhor para perceber os aromas"
+    },
+    {
+        "q": "As videiras da região de Colares (Portugal) são plantadas em qual tipo de solo?"
+        , "a": "Area de dunas"
+    },
+    {
+        "q": "Qual o nome da Praga que devastou vinhedos no mundo?"
+        , "a": "Filoxera"
+    },
+    {
+        "q": "O que significa um corte GSM?"
+        , "a": "Grenache, Syrah e Mourvèdre, 3 uvas formando GSM"
+    },
+    {
+        "q": "Qual das regiões abaixo é uma Denominação de Origem da Espanha?"
+        , "a": "Ribera del Duero"
+    },
+    {
+        "q": "A Alemanha possui quantas regiões vinícolas?"
+        , "a": "13 regiões"
+    },
+    {
+        "q": "Quantas AVAs possui Napa Valley?"
+        , "a": "16"
+    },
+    {
+        "q": "O Vale dos vinhedos fica localizado em qual destas regiões?"
+        , "a": "Serra Gaúcha"
+    },
+    {
+        "q": "Qual uva é mais plantada na Margem Esquerda de Bordeaux?"
+        , "a": "Cabernet Sauvignon"
+    },
+    {
+        "q": "Quem é o enólogo no mundo do vinho?"
+        , "a": "Profissional responsável pelo processo de vinificação"
+    },
+    {
+        "q": "Como se pronuncia a uva Chardonnay?"
+        , "a": "Chardonê"
+    },
+    {
+        "q": "A uva Pinotage é um cruzamento das uvas:"
+        , "a": "Pinot noir e Hermitage (Cinsault)"
+    },
+    {
+        "q": "Queijos duros como o parmesão combinam com quais TINTOS?"
+        , "a": "Chianti, Barolo, Brunello"
+    },
+    {
+        "q": 'O que significa o termo "chevillé"?'
+        , "a": "A rolha de cortiça mais velha que não expande ao abrir a garrafa"
+    },
+    {
+        "q": "Coonawarra é uma região vitivinícola de que país?"
+        , "a": "Austrália"
+    },
+    {
+        "q": "Qual a principal característica da acidez do vinho no paladar?"
+        , "a": "Salivação"
+    },
+    {
+        "q": 'O que significa o termo "juponé"?'
+        , "a": "A rolha quando expande depois de abrir a garrafa com formato de cogumelo"
+    },
+    {
+        "q": "O que é Prosecco?"
+        , "a": "Um vinho espumante produzido nas regiões de Veneto e Friuli-Venezia Giulia, na Itália"
+    },
+    {
+        "q": "Qual abaixo é uma uva TINTA do Sul do Rhône?"
+        , "a": "Grenache Noir"
+    },
+    {
+        "q": "Qual a maneira correta de abrir um espumante?"
+        , "a": "Segurando a garrafa na diagonal, longe do rosto, segurando a rolha para sair aos poucos"
+    },
+    {
+        "q": "Assinale a combinação ERRADA de país e uva:"
+        , "a": "Argentina - Pinot Noir"
+    }
+];
